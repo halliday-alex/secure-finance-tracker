6003943085d331667901b69b2a0748414daf4322
halliday-alex <halliday_a@icloud.com>
2026-06-19 08:46:22 +0100

    Complete PostgreSQL connection and verify schema

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 #FastAPI handles web routing, incoming requests and builds the auto-documentation.
 from fastapi import FastAPI
+from src.database.connection import engine,Base #engine is the bridge to postgres, Base is database blueprint metadata.
+from src.database import models #import  models file so SQLAlchemy is aware of users, transactions, budgets
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Secure Finance Tracker API",description="A Cyber-secure backend framework for managing finance.",
               version="1.0.0")
